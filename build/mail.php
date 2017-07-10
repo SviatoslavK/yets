48fb6f007fad1d01b10a902f2f632cb0d6218c38
Slava <kolodaslav@gmail.com>
2017-07-10 23:19:01 +0300

    +++

---
+++ build/mail.php
@@ -0,0 +1,9 @@
+
+<?php
+$recepient = "kolodaslav@gmail.ua";
+$sitename = "YETS";
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+$pagetitle = "Новая заявка с сайта \"$YETS\"";
+$message = "Имя: $name \nТелефон: $phone "; 
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -26,7 +26,7 @@
 * @param {(string)} [options.arrNextText=&raquo;] - Text for _NEXT_ arrow.
 */
 function Carousel(options) {
-    var element  = document.getElementById(options.elem || 'carousel'),
+    var element = document.getElementById(options.elem || 'carousel'),
         interval = options.interval || 3000,
 
         btnPlayText = options.btnPlayText || 'Play',
@@ -35,16 +35,16 @@ function Carousel(options) {
         arrNextText = options.arrNextText || '&rsaquo;',
         arrPrevText = options.arrPrevText || '&lsaquo;',
 
-        crslClass           = 'js-Carousel',
-        crslArrowPrevClass  = 'js-Carousel-arrowPrev',
-        crslArrowNextClass  = 'js-Carousel-arrowNext',
-        crslDotsClass       = 'js-Carousel-dots',
+        crslClass = 'js-Carousel',
+        crslArrowPrevClass = 'js-Carousel-arrowPrev',
+        crslArrowNextClass = 'js-Carousel-arrowNext',
+        crslDotsClass = 'js-Carousel-dots',
         crslButtonStopClass = 'js-Carousel-btnStop',
         crslButtonPlayClass = 'js-Carousel-btnPlay',
 
-        count   = element.querySelectorAll('li').length,
+        count = element.querySelectorAll('li').length,
         current = 0,
-        cycle   = null;
+        cycle = null;
 
     /**
     * Render the carousel if more than one slide. 
@@ -61,22 +61,22 @@ function Carousel(options) {
     */
     function render() {
         var actions = {
-            dots: function() {
+            dots: function () {
                 return showDots();
             },
-            arrows: function() {
+            arrows: function () {
                 return showArrows();
             },
-            buttons: function() {
+            buttons: function () {
                 return showButtons();
             },
-            autoplay: function() {
+            autoplay: function () {
                 return play();
             },
-            infinite: function() {
+            infinite: function () {
                 return moveItem(count - 1, -element.offsetWidth + 'px', 'afterBegin');
             },
-            initial: function() {
+            initial: function () {
                 var initial = 0 || (options.initial >= count) ? count : options.initial;
                 return show(initial);
             }
@@ -104,10 +104,10 @@ function Carousel(options) {
         itemToMove.style.marginLeft = marginLeft;
 
         element.querySelector('.' + crslClass + ' > ul')
-          .removeChild(itemToMove);
+            .removeChild(itemToMove);
 
         element.querySelector('.' + crslClass + ' > ul')
-          .insertAdjacentHTML(position, itemToMove.outerHTML);
+            .insertAdjacentHTML(position, itemToMove.outerHTML);
     }
 
     /**
@@ -133,7 +133,7 @@ function Carousel(options) {
     * Highlight the corresponding dot of the currently visible carousel item.
     */
     function currentDot() {
-        [].forEach.call(element.querySelectorAll('.' + crslDotsClass + ' li'), function(item) {
+        [].forEach.call(element.querySelectorAll('.' + crslDotsClass + ' li'), function (item) {
             item.classList.remove('is-active');
         });
 
@@ -280,7 +280,7 @@ function Carousel(options) {
             return;
         }
         animatePrev(document.querySelectorAll('.' + crslClass + ' > ul li')[current - 1]);
-        
+
         adjustCurrent(-1);
     }
 
@@ -384,7 +384,7 @@ function Carousel(options) {
 // //header img 
 let headerBlock = document.querySelector('.header');
 let headerImg = document.querySelector('.header__images');
-    headerImg.style.left = -(1920 - headerBlock.clientWidth)/2 + 'px';
+headerImg.style.left = -(1920 - headerBlock.clientWidth) / 2 + 'px';
 
 
 var carousel = new Carousel({
@@ -398,247 +398,6 @@ var carousel = new Carousel({
     buttons: false,      // hide play/stop buttons,
     btnStopText: 'Pause' // STOP button text
 });
-// // Slider 
-// (function () {
-//     "use strict";
-
-//     function Carousel(setting) {
-//         if (document.querySelector(setting.wrap) === null) {
-//             console.error(`Carousel not fount selector ${setting.wrap}`);
-//             return;
-//         }
-
-//         /* Scope privates methods and properties */
-//         let privates = {},
-//             xDown, yDown, xUp, yUp, xDiff, yDiff;
-
-//         /* Public methods */
-//         // Prev slide
-//         this.prev_slide = () => {
-//             if (!privates.isAnimationEnd) {
-//                 return;
-//             }
-
-//             privates.isAnimationEnd = false;
-
-//             --privates.opt.position;
-
-//             if (privates.opt.position < 0) {
-//                 privates.sel.wrap.classList.add('s-notransition');
-//                 privates.sel.wrap.style["transform"] = `translateX(-${privates.opt.max_position}00%)`;
-//                 privates.opt.position = privates.opt.max_position - 1;
-//             }
-
-//             setTimeout(() => {
-//                 privates.sel.wrap.classList.remove('s-notransition');
-//                 privates.sel.wrap.style["transform"] = `translateX(-${privates.opt.position}00%)`;
-//             }, 10);
-
-//             privates.sel.wrap.addEventListener('transitionend', () => {
-//                 privates.isAnimationEnd = true;
-//             });
-
-//             if (privates.setting.autoplay === true) {
-//                 privates.timer.become();
-//             }
-//         };
-
-//         // Next slide
-//         this.next_slide = () => {
-//             if (!privates.isAnimationEnd) {
-//                 return;
-//             }
-
-//             privates.isAnimationEnd = false;
-
-//             if (privates.opt.position < privates.opt.max_position) {
-//                 ++privates.opt.position;
-//             }
-
-//             privates.sel.wrap.classList.remove('s-notransition');
-//             privates.sel.wrap.style["transform"] = `translateX(-${privates.opt.position}00%)`;
-
-//             privates.sel.wrap.addEventListener('transitionend', () => {
-//                 if (privates.opt.position >= privates.opt.max_position) {
-//                     privates.sel.wrap.style["transform"] = 'translateX(0)';
-//                     privates.sel.wrap.classList.add('s-notransition');
-//                     privates.opt.position = 0;
-//                 }
-
-//                 privates.isAnimationEnd = true;
-//             });
-
-//             if (privates.setting.autoplay === true) {
-//                 privates.timer.become();
-//             }
-//         };
-
-//         // Pause timer carousel
-//         this.pause = () => {
-//             if (privates.setting.autoplay === true) {
-//                 privates.timer.pause();
-//             }
-//         };
-
-//         // Become timer carousel
-//         this.become = (autoplayDelay = privates.setting.autoplayDelay) => {
-//             if (privates.setting.autoplay === true) {
-//                 privates.setting.autoplayDelay = autoplayDelay;
-//                 privates.timer.become();
-//             }
-//         };
-
-//         // Go to
-//         this.goto = (index) => {
-//             privates.opt.position = index - 1;
-//             this.next_slide();
-//         };
-
-//         // Item
-//         this.index = () => {
-//             return privates.opt.position;
-//         };
-
-//         /* privates methods */
-//         privates.hts = (e) => {
-//             xDown = e.touches[0].clientX;
-//             yDown = e.touches[0].clientY;
-//         };
-
-//         privates.htm = (e) => {
-//             if (!xDown || !yDown) {
-//                 return;
-//             }
-
-//             xUp = e.touches[0].clientX;
-//             yUp = e.touches[0].clientY;
-
-//             xDiff = xDown - xUp;
-//             yDiff = yDown - yUp;
-
-//             if (Math.abs(xDiff) > Math.abs(yDiff)) {
-//                 if (xDiff > 0) {
-//                     this.next_slide();
-//                 } else {
-//                     this.prev_slide();
-//                 }
-//             }
-
-//             xDown = 0;
-//             yDown = 0;
-//         }
-
-//         /* privates properties */
-//         privates.default = {
-//             "touch": true,
-//             "autoplay": false,
-//             "autoplayDelay": 3000,
-//             "pauseOnFocus": true,
-//             "pauseOnHover": true
-//         };
-
-//         privates.setting = Object.assign(privates.default, setting);
-
-//         privates.isAnimationEnd = true;
-
-//         privates.sel = {
-//             "wrap": document.querySelector(privates.setting.wrap),
-//             "children": document.querySelector(privates.setting.wrap).children,
-//             "prev": document.querySelector(privates.setting.prev),
-//             "next": document.querySelector(privates.setting.next)
-//         };
-
-//         privates.opt = {
-//             "position": 0,
-//             "max_position": document.querySelector(privates.setting.wrap).children.length
-//         };
-
-
-//         /* Constructor */
-//         // Clone first elem to end wrap
-//         privates.sel.wrap.appendChild(privates.sel.children[0].cloneNode(true));
-
-
-//         // Autoplay
-//         if (privates.setting.autoplay === true) {
-//             privates.timer = new Timer(this.next_slide, privates.setting.autoplayDelay);
-//         }
-
-
-//         // Control
-//         if (privates.sel.prev !== null) {
-//             privates.sel.prev.addEventListener('click', () => {
-//                 this.prev_slide();
-//             });
-//         }
-
-//         if (privates.sel.next !== null) {
-//             privates.sel.next.addEventListener('click', () => {
-//                 this.next_slide();
-//             });
-//         }
-
-//         // Touch events
-//         if (privates.setting.touch === true) {
-//             privates.sel.wrap.addEventListener('touchstart', privates.hts, false);
-//             privates.sel.wrap.addEventListener('touchmove', privates.htm, false);
-//         }
-
-//         // Pause on hover
-//         if (privates.setting.autoplay === true && privates.setting.pauseOnHover === true) {
-//             privates.sel.wrap.addEventListener('mouseenter', () => {
-//                 privates.timer.pause();
-//             });
-
-//             privates.sel.wrap.addEventListener('mouseleave', () => {
-//                 privates.timer.become();
-//             });
-//         }
-
-
-//     }
-
-//     function Timer(callback, delay) {
-//         /* privates properties */
-//         let timerId, start, remaining = delay;
-
-//         /* Public methods */
-//         this.resume = () => {
-//             start = new Date();
-//             timerId = setTimeout(() => {
-//                 remaining = delay;
-//                 this.resume();
-//                 callback();
-//             }, remaining);
-//         };
-
-//         this.pause = () => {
-//             clearTimeout(timerId);
-//             remaining -= new Date() - start;
-//         };
-
-//         this.become = () => {
-//             clearTimeout(timerId);
-//             remaining = delay;
-
-//             this.resume();
-//         };
-
-//         /* Constructor */
-//         this.resume();
-//     }
-
-//     let a = new Carousel({
-//         "wrap": ".js-carousel__wrap",
-//         "prev": ".js-carousel__prev",
-//         "next": ".js-carousel__next",
-//         "touch": true,
-//         "autoplay": true,
-//         "autoplayDelay": 3000
-//     });
-
-// })();
-
 
 let avatars = [...document.querySelectorAll('.client__ava')];
 let layout = document.querySelector('.clients');
@@ -658,18 +417,15 @@ const hoverAva = (e) => {
         if (!ava.hasAttribute('checked')) {
             ava.setAttribute('checked', '')
             ava.appendChild(overlay);
-            fL.style.borderColor = 'transparent'
-            sL.style.borderColor = 'transparent'
-            tL.style.borderColor = 'transparent'
+            fL.classList.add('border__transparent')
+            sL.classList.add('border__transparent')
+            tL.classList.add('border__transparent')
             otherEl.map(el => el.parentElement.parentElement.parentElement.parentElement.classList.add('hiddensss'));
-        } 
-        // else {
-        //     ava.removeAttribute('checked')
-        //     document.querySelector('.comment__overlay').remove();
-        //     otherEl.map(el => el.parentElement.parentElement.parentElement.parentElement.classList.remove('hiddensss'))
-        // }
+        }
     }
 }
+
+
 avatars.map(a => {
     a.addEventListener('click', hoverAva);
 })
@@ -678,9 +434,9 @@ const disableComment = () => {
     avatars.map(el => {
         el.removeAttribute('checked');
         el.parentElement.parentElement.parentElement.parentElement.classList.remove('hiddensss');
-        el.parentElement.parentElement.parentElement.style.borderColor = '#E6E6E6'
-        el.parentElement.parentElement.style.borderColor = '#FF0A44'
-        el.parentElement.style.borderColor = '#C8C8C8'
+        el.parentElement.parentElement.parentElement.classList.remove('border__transparent')
+        el.parentElement.parentElement.classList.remove('border__transparent')
+        el.parentElement.classList.remove('border__transparent')
     })
 }
 document.addEventListener('click', (e) => {
@@ -689,3 +445,45 @@ document.addEventListener('click', (e) => {
     }
 })
 
+// const formData = (form) => {
+//     return 
+// }
+
+// Send mail header__form start__form 
+// let headerForm = document.querySelector('#header__form')
+// let startForm = document.querySelector('#start__form')
+// let footerForm = document.querySelector('#footer__form')
+// let headerFormBtn = headerForm.querySelector('.form__btn')
+
+$(document).ready(function () {
+    $("#header__form").submit(function () {
+          $.ajax({
+               type: "GET",
+               url: "../mail.php",
+               data: $("#header__form").serialize()
+          }).done(function () {
+               alert("Спасибо за заявку!");
+          });
+          return false;
+     });
+    $("#start__form").submit(function () {
+          $.ajax({
+               type: "GET",
+               url: "../mail.php",
+               data: $("#start__form").serialize()
+          }).done(function () {
+               alert("Спасибо за заявку!");
+          });
+          return false;
+     });
+    $("#footer__form").submit(function () {
+          $.ajax({
+               type: "GET",
+               url: "../mail.php",
+               data: $("#footer__form").serialize()
+          }).done(function () {
+               alert("Спасибо за заявку!");
+          });
+          return false;
+     });
+});
